5ea07bb3a38918402873b178e5e6889b29d4b979
SaumitraLohokare <saumitra.lohokare@gmail.com>
2025-02-02 21:53:39 +0530

    Added another example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use lube::ir::{Module, Function, Size, Value};
+use lube::ir::{Function, Module, Size, Value};
 
 fn main() -> io::Result<()> {
     /*
@@ -63,8 +63,8 @@ fn main() -> io::Result<()> {
     module.generate_asm().save_to(".build/add.s")?;
 
     /*
-    
-        void why_would_you_do_this(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j) 
+
+        void why_would_you_do_this(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j)
         {
         }
 
@@ -72,7 +72,7 @@ fn main() -> io::Result<()> {
 
         why_would_you_do_this:
             return
-    
+
     */
 
     let mut module = Module::new();
@@ -94,10 +94,12 @@ fn main() -> io::Result<()> {
 
     module.add_func(func);
 
-    module.generate_asm().save_to(".build/why_would_you_do_this.s")?;
+    module
+        .generate_asm()
+        .save_to(".build/why_would_you_do_this.s")?;
 
     /*
-    
+
         void variables() {
             char a = 33;
             unsigned short b = 69;
@@ -116,7 +118,7 @@ fn main() -> io::Result<()> {
             $2 = %2
             %3 = 9876543210
             $3 = %3
-    
+
     */
 
     let mut module = Module::new();
@@ -138,7 +140,7 @@ fn main() -> io::Result<()> {
     module.generate_asm().save_to(".build/variables.s")?;
 
     /*
-    
+
         short signed_add(short a, short b) {
             short c = a + b;
             return c;
@@ -153,7 +155,7 @@ fn main() -> io::Result<()> {
             $2 = %2
             %3 = $2
             return %3
-    
+
     */
 
     let mut module = Module::new();
@@ -174,13 +176,13 @@ fn main() -> io::Result<()> {
     module.add_func(func);
 
     module.generate_asm().save_to(".build/signed_add.s")?;
-    
+
     /*
 
         int add(int a, int b) {
             return a + b;
         }
-    
+
         int main(void) {
             int a = 2;
             int b = -1;
@@ -205,7 +207,7 @@ fn main() -> io::Result<()> {
             call add %2 %3
             %4 = call_result
             return %4
-    
+
     */
 
     let mut module = Module::new();
@@ -240,6 +242,85 @@ fn main() -> io::Result<()> {
 
     module.generate_asm().save_to(".build/func_call.s")?;
 
+    /*
+
+        void why_would_you_do_this(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j)
+        {
+        }
+
+        int main() {
+            why_would_you_do_this(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+            return 0
+        }
+
+        ==========
+
+        why_would_you_do_this:
+            return
+
+        main:
+            %0 = 0
+            %1 = 1
+            %2 = 2
+            %3 = 3
+            %4 = 4
+            %5 = 5
+            %6 = 6
+            %7 = 7
+            %8 = 8
+            %9 = 9
+            call why_would_you_do_this %0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+            %10 = 0
+            return %10
+
+    */
+
+    let mut module = Module::new();
+
+    let mut func = Function::new("_why_would_you_do_this".to_string());
+
+    func.add_arg(Size::DoubleWord, true);
+    func.add_arg(Size::DoubleWord, true);
+    func.add_arg(Size::DoubleWord, true);
+    func.add_arg(Size::DoubleWord, true);
+    func.add_arg(Size::DoubleWord, true);
+    func.add_arg(Size::DoubleWord, true);
+    func.add_arg(Size::DoubleWord, true);
+    func.add_arg(Size::DoubleWord, true);
+    func.add_arg(Size::DoubleWord, true);
+    func.add_arg(Size::DoubleWord, true);
+
+    func.add_inst_return(None);
+
+    module.add_func(func);
+
+    let mut func = Function::new("_main".to_string());
+
+    let tmp_0 = func.add_inst_set(Value::I32(0));
+    let tmp_1 = func.add_inst_set(Value::I32(1));
+    let tmp_2 = func.add_inst_set(Value::I32(2));
+    let tmp_3 = func.add_inst_set(Value::I32(3));
+    let tmp_4 = func.add_inst_set(Value::I32(4));
+    let tmp_5 = func.add_inst_set(Value::I32(5));
+    let tmp_6 = func.add_inst_set(Value::I32(6));
+    let tmp_7 = func.add_inst_set(Value::I32(7));
+    let tmp_8 = func.add_inst_set(Value::I32(8));
+    let tmp_9 = func.add_inst_set(Value::I32(9));
+    func.add_inst_call(
+        "_why_would_you_do_this".to_string(),
+        vec![
+            tmp_0, tmp_1, tmp_2, tmp_3, tmp_4, tmp_5, tmp_6, tmp_7, tmp_8, tmp_9,
+        ],
+    );
+
+    let tmp_10 = func.add_inst_set(Value::I32(0));
+    func.add_inst_return(Some(tmp_10));
+
+    module.add_func(func);
+
+    module
+        .generate_asm()
+        .save_to(".build/why_would_you_do_this2.s")?;
 
     Ok(())
 }
